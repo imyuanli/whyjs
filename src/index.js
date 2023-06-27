fcf5927cd9a01cb9207d79fa0a9276a787202c4d
yuanli <2865437316@qq.com>
2023-06-27 15:58:36 +0800

    feat: 新增数学计算相关方法

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 export * from './array'
 export * from './judgment'
+export * from './math'
 
 
 import * as array from './array'
 import * as judgment from './judgment'
+import * as math from './math'
 
 
 export const y = {
   ...array,
   ...judgment,
+  ...math
 }
