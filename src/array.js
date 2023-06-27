750699ceed322074f733f05fccc7b3ecc02b06b6
yuanli <2865437316@qq.com>
2023-06-27 18:08:41 +0800

    feat: 新增 pull without uniq

--- src/array.js
+++ src/array.js
@@ -12,3 +12,14 @@ export const chunk = (array, size = 1) => {
 }
 export const compact = array => isArray(array) ? array.filter(Boolean) : []
 export const concat = (array, ...values) => isArray(array) ? [...array, ...values] : []
+export const pull = (array, ...values) => {
+  if (!isArray(array)) return [];
+  for (let i = 0; i < array.length; i++) {
+    if (values.includes(array[i])) {
+      array.splice(i, 1);
+      i--;
+    }
+  }
+}
+export const without = (array, ...values) => isArray(array) ? array.filter(item => !values.includes(item)) : []
+export const uniq = array => isArray(array) ? [...new Set(array)] : []
