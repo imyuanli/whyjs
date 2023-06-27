4254347507644c8cb7e9142e0ee66652cd824790
yuanli <2865437316@qq.com>
2023-06-27 15:58:22 +0800

    feat: 新增判断相关方法

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 export * from './array'
+export * from './judgment'
+
 
 import * as array from './array'
+import * as judgment from './judgment'
+
 
 export const y = {
   ...array,
+  ...judgment,
 }
